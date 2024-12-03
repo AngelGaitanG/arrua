c5236fc53f1a51d2064cce60971249385def9c45
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-12-02 22:18:59 -0300

    Update About section with additional mission and vision statements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { HeroComponent } from './features/hero/hero.component';
 import { ServicesComponent } from './features/services/services.component';
 import { CommonModule } from '@angular/common';
+import { AboutComponent } from './features/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, ServicesComponent, CommonModule],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, ServicesComponent, CommonModule, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
