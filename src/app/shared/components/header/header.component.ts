b5b6a7d0b0142469f964cdbfbddff3ca975edb97
Angel Gaitan <angel.gaitan.ev@gmail.com>
2024-12-29 10:34:14 -0300

    ready for deploy

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { NgClass, NgFor } from '@angular/common';
 
 interface NavLink {
   text: string;
@@ -11,21 +11,27 @@ interface NavLink {
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   standalone: true,
-  imports: [NgFor]
+  imports: [NgFor, NgClass]
 })
 export class HeaderComponent {
   navLinks: NavLink[] = [
-    { text: 'Sobre nosotros', url: '#' },
-    { text: 'Sobre la clínica', url: '#' },
-    { text: 'Especialistas', url: '#' },
-    { text: 'Comentarios', url: '#' },
-    { text: 'Contactos', url: '#' }
+    { text: 'Sobre nosotros', url: '#about' },
+    { text: 'Servicios', url: '#services' },
+    { text: 'Especialistas', url: '#doctors' },
+    { text: 'Comentarios', url: '#reviews' },
+    { text: 'Contacto', url: '#contact' }
   ];
 
   phoneNumber = '2664647228';
   menuOpen = false;
+  isScrolled = false;
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50;
   }
 }
